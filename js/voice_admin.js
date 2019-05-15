20dd47c9fb34223fba36283b5088affd3d715e7b
Ted Bowman <ted@tedbow.com>
2019-05-15 09:38:26 -0400

    add back hard coded commands

--- js/voice_admin.js
+++ js/voice_admin.js
@@ -7,17 +7,32 @@ recognition.addEventListener('result', e => {
   transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join('');
   const statement = e.results[0][0].transcript;
   console.log(statement);
+  // All commnand must start with "voice admin"
   if (statement.startsWith('voice admin ')) {
     const command = statement.replace('voice admin ', '');
+    // Link command.
     if (command.startsWith('link ')) {
       const linkText = command.replace('link ', '');
       for (const a of document.querySelectorAll("a")) {
-        if (a.textContent.toLowerCase() == linkText) {
+        if (a.textContent.toLowerCase() === linkText) {
           a.click();
         }
       }
+      return;
+    }
+    // Hardcoded commands.
+    if (command === "new page") {
+      window.location.href = "/node/add/page";
+    } else if (command === "new article") {
+      window.location.href = "/node/add/article";
+    } else if (command === "log out") {
+      window.location.href = "/user/logout";
+    } else if (command === "go home") {
+      window.location.href = "/en";
     }
   }
+  //
+
 
 });
 
